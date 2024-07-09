bde43388b4b92287b814dde4cdf55afdda293cba
Given Ncube <givenyslim12@gmail.com>
2024-07-09 23:26:16 +0200

    feat: added the ability to customize behaviour

--- config/bacs.php
+++ config/bacs.php
@@ -2,5 +2,17 @@
 
 // config for LaravelBacs/LaravelBacs
 return [
+    /*
+     * -----------------------
+     *
+     * The route for the BACS endpoint
+     */
+    'route' => 'api/bacs',
 
+    /*
+     * -------------------------
+     *
+     * Register any middleware you'd like to run before this route
+     */
+    'middleware' => [],
 ];
